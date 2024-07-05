f33bff26fc449217464c2605b4bbd70e0a16d775
Marcus Ziadé <guitaripod@icloud.com>
2024-07-04 18:19:44 -0700

    init swift package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "SteamKit",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v8),
+    ],
+    products: [
+        .library(
+            name: "SteamKit",
+            targets: ["SteamKit"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SteamKit",
+            dependencies: []),
+        .testTarget(
+            name: "SteamKitTests",
+            dependencies: ["SteamKit"]
+        ),
+    ]
+)
